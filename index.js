bd1498a0d4395b369b4b734da5dd52fe32805374
Smriti055 <161960307+Smriti055@users.noreply.github.com>
2025-03-08 00:50:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cors(
     {
-        origin: "https://app.netlify.com/sites/roaring-buttercream-4a5966/deploys/67cb416a74cef955036e8861",
+        origin: "https://roaring-buttercream-4a5966.netlify.app",
         credentials: true
     }
 ));
